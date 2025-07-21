ad26d5f95e23a6f81fa8c3305f4c0467f9fad320
Hienhuynh4436 <hienhuynh111.hh@gmail.com>
2025-07-21 21:23:09 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  logoUrl: { type: String }
+  logoUrl: { type: String }, 
+  lastLogin: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model("User", userSchema);
