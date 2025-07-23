7937acb8ee6063a0cb49835ac67fa59a8df5da3a
Hienhuynh4436 <hienhuynh111.hh@gmail.com>
2025-07-23 19:52:10 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isVerified: { type: Boolean, default: true },
   preferences: {
-  bgColor: String,
-  textColor: String,
-  borderColor: String,
-  titleColor: String,
-  fontFamily: String
-},
-  logoUrl: { type: String }, 
-  lastLogin: { type: Date, default: Date.now }, 
+    bgColor: String,
+    textColor: String,
+    borderColor: String,
+    titleColor: String,
+    fontFamily: String
+  },
+  logoUrl: { type: String },
+  lastLogin: { type: Date, default: Date.now },
   resetToken: String,
   resetTokenExpiry: Date
 });
+
 module.exports = mongoose.model("User", userSchema);
