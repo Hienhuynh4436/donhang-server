28a5e53bad696842b09a950d72be284b25d15e9d
Hienhuynh4436 <hienhuynh111.hh@gmail.com>
2025-07-23 19:31:27 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  verifyToken: String,
-  verifyExpires: Date,    
+  isVerified: { type: Boolean, default: true },
   preferences: {
   bgColor: String,
   textColor: String,
